05da116df0a9bb5db7f63357f6b8a53ba1d380ea
sankhoroy <sankhoroy@gmail.com>
2017-10-16 12:48:13 +0530

    test cases modified

--- src/test/java/com/stackroute/newsaggregatortest/ApplicationTest.java
+++ src/test/java/com/stackroute/newsaggregatortest/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.stackroute.Useraggregatortest;
+package com.stackroute.Usertest;
 
 
 import junit.framework.TestCase;
@@ -21,80 +21,85 @@ import com.stackroute.hackathon.domain.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest  extends TestCase {
+ 
+    @LocalServerPort
+    private int port;
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+    User user;    
    
-	@LocalServerPort
-	private int port;
-	TestRestTemplate restTemplate = new TestRestTemplate();
-	HttpHeaders headers = new HttpHeaders();
-	User User;    
+    @Before
+   public void setUp() throws Exception {
+        user = new User();
+        user.setId(1);
+        user.setEmailId("abhi28@gmail.com");
+        user.setUsername("abhijit");
+   }
     
-	@Before
-    public void setUp() throws Exception {
-		User = new User(1,"abhi28@gmail.com","abhijit");
-    }
-	
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
-    
-    @After
-    public void tearDown() throws Exception {
-    }
-    
-    @Test
-    public void testSaveUser() throws Exception { 
+   private String createURLWithPort(String uri) {
+       return "http://localhost:" + port + uri;
+   }
+   
+   @After
+   public void tearDown() throws Exception {
+       user=null;
+   }
+   
+   @Test
+   public void testSaveUser() throws Exception {
 
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<User> entity = new HttpEntity<User>(User, headers); 
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/Users/user"),
-                HttpMethod.POST, entity, String.class); 
-        assertNotNull(response);
-        String actual = response.getBody();
-        System.out.println(actual);
-        assertEquals("User profile Saved",actual);
-    }
-    
-    @Test
-    public void testList() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/Users"),
-                HttpMethod.GET, entity, String.class);
-        assertNotNull(response);
-    }
-    
-    @Test
-    public void testGetUser() throws Exception {
-    }
-    
-    @Test
-    public void testUpdateUser() throws Exception {
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<User> entity = new HttpEntity<User>(User, headers); 
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/Users/1?name=abhijit&email=abhi28@gmail.com"),
-                HttpMethod.PUT, entity, String.class); 
-        assertNotNull(response);
-        String actual = response.getBody();
-        System.out.println(actual);
-        assertEquals("Updated",actual);
-    }
-    
-    @Test
-    public void testDeleteUser() throws Exception {
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<User> entity = new HttpEntity<User>(User, headers); 
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/Users/delete/1"),
-                HttpMethod.DELETE, entity, String.class); 
-        assertNotNull(response);
-        String actual = response.getBody();
-        System.out.println(actual);
-        assertEquals("User Deleted",actual);
-    }
-    
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       HttpEntity<User> entity = new HttpEntity<User>(user, headers);
+       ResponseEntity<String> response = restTemplate.exchange(
+               createURLWithPort("/Users/user"),
+               HttpMethod.POST, entity, String.class);
+       assertNotNull(response);
+       String actual = response.getBody();
+       System.out.println(actual);
+       assertEquals("User Profile Saved",actual);
+   }
+   
+   @Test
+   public void testList() throws Exception {
+       HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+       ResponseEntity<String> response = restTemplate.exchange(
+               createURLWithPort("/Users"),
+               HttpMethod.GET, entity, String.class);
+       assertNotNull(response);
+   }
+   
+   @Test
+   public void testGetUser() throws Exception {
+   }
+   
+   @Test
+   public void testUpdateUser() throws Exception {
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       HttpEntity<User> entity = new HttpEntity<User>(user, headers);
+       ResponseEntity<String> response = restTemplate.exchange(
+               createURLWithPort("/Users/1?name=abhijit&email=abhi28@gmail.com"),
+               HttpMethod.PUT, entity, String.class);
+       assertNotNull(response);
+       String actual = response.getBody();
+       System.out.println(actual);
+       assertEquals("Updated",actual);
+   }
+   
+   @Test
+   public void testDeleteUser() throws Exception {
+       System.out.println(" sankho"+user.getId());
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       HttpEntity<User> entity = new HttpEntity<User>(user, headers);
+       ResponseEntity<String> response = restTemplate.exchange(
+               createURLWithPort("/Users/delete/1"),
+               HttpMethod.DELETE, entity, String.class);
+       assertNotNull(response);
+       String actual = response.getBody();
+       System.out.println(actual);
+       assertEquals("User Deleted",actual);
+   }
+   
 }
